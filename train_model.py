2033544a8b7d6da56086bd58f5d189219508b025
quintenrosseel <quintenrosseel>
2023-01-22 20:58:27 +0000

    Added model training script

---
+++ train_model.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+# MAGIC %md 
+# MAGIC 
+# MAGIC #Model Training
+# MAGIC 
+# MAGIC This notebook trains various models for the GA + Linkedin Analytics dataset, using MLflow API.  
+
+# COMMAND ----------
+
+dbutils.fs.ls('/user/hive/warehouse') 
+
+# COMMAND ----------
+
+
